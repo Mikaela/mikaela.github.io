f6e141442633508cfb2d716fb1e7a0ea626ce7c5
Mikaela Suomalainen <mikaela.suomalainen@outlook.com>
2014-04-02 10:33:49 +0300

    head.php: convert head.html to php
    So I can cheat and copy-paste easily to my PHP.

---
+++ head.php
@@ -0,0 +1,12 @@
+<?php
+echo '<!DOCTYPE html>';
+echo '<html>';
+echo '<head>';
+echo '<meta charset="UTF-8" />';
+echo '<meta name="description" content="Insert description here" />';
+echo '<meta name="keywords" content="Insert,keywords,here" />';
+echo '<meta name="author" content="Mikaela Suomalainen" />';
+echo '<title>Insert title here</title>';
+echo '</head>';
+
+?>
