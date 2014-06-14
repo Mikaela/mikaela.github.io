6bbb6b8955800f6d488c76fe38173ff973b92f01
Mikaela Suomalainen <mikaela.suomalainen@outlook.com>
2014-06-14 20:15:08 +0300

    Fix IPTITLE.php

--- IPTITLE.php
+++ IPTITLE.php
@@ -4,6 +4,10 @@ header("Content-Type: text/plain; charset=utf-8");
 
 $IP = $_SERVER['REMOTE_ADDR'];
 
+echo '<!DOCTYPE html>';
+echo '<head>';
+echo '<meta charset="UTF-8" />';
 echo "<title>$IP</title>";
-
+echo '</head>';
+echo '</html>';
 ?>
