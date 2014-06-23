f78069c4f08dce1f92c72fe093e011e101367166
Mikaela Suomalainen <mikaela.suomalainen@outlook.com>
2014-06-23 17:30:48 +0300

    Create new folder php/ and put php files there.
    * Also put symlinks for backwards compatibility.

--- IP.php
+++ IP.php
@@ -1,9 +1,1 @@
-<?php
-
-header("Content-Type: text/plain; charset=utf-8");
-
-$IP = $_SERVER['REMOTE_ADDR'];
-
-echo "$IP";
-
-?>
+php/IP.php
--- IPTITLE.php
+++ IPTITLE.php
@@ -1,13 +1,1 @@
-<?php
-
-header("Content-Type: text/html; charset=utf-8");
-
-$IP = $_SERVER['REMOTE_ADDR'];
-
-echo '<!DOCTYPE html>';
-echo '<head>';
-echo '<meta charset="UTF-8" />';
-echo "<title>$IP</title>";
-echo '</head>';
-echo '</html>';
-?>
+php/IPTITLE.php
